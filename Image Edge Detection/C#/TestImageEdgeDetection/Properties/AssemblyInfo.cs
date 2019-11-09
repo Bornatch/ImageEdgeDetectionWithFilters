404a381703289b0e267a55322d3b9419b4a23b56
Nicolas Solioz <nicolas.solioz@students.hevs.ch>
2019-11-09 10:16:58 +0100

    Add Project Test
    For UnitTesting

---
+++ Image Edge Detection/C#/TestImageEdgeDetection/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestImageEdgeDetection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestImageEdgeDetection")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9c63572-0416-4ec4-8458-6d420304f2a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Image Edge Detection/C#/TestImageEdgeDetection/TestImageFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestImageEdgeDetection
+{
+    [TestClass]
+    public class TestImageFilter
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
